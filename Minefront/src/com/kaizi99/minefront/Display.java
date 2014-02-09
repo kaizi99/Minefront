3e3287fb0e5d5ce5778743b2c278ab7805e30a33
kaizi99 <LPinDE@outlook.com>
2014-02-09 10:23:26 +0100

    Display updated, Episode 3

--- Minefront/src/com/kaizi99/minefront/Display.java
+++ Minefront/src/com/kaizi99/minefront/Display.java
@@ -1,15 +1,50 @@
 package com.kaizi99.minefront;
 
 import java.awt.Canvas;
+
 import javax.swing.JFrame;
 
-public class Display  extends Canvas{
+public class Display extends Canvas implements Runnable{
 
 	public static final int WIDTH = 800;
 	public static final int HEIGHT = 600;
 	public static final String TITLE = "Minefront - PRE ALPHA 0.0.1";
-	
+
 	private Thread threads;
+	private boolean running = false;
+
+	private void start() {
+		if (running) {
+			return;
+		}
+		
+		running = true;
+		threads = new Thread(this);
+		threads.start();
+		
+		System.out.println("WORKING!!!");
+	}
+
+	private void stop() {
+		if(!running) {
+			return;
+		}
+		
+		running = false;
+		
+		try {
+			threads.join();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	public void run() {
+		while(running) {
+			
+		}
+	}
 	
 	public static void main(String[] args) {
 		Display game = new Display();
@@ -22,6 +57,7 @@ public class Display  extends Canvas{
 		frame.setResizable(false);
 		frame.setVisible(true);
 		System.out.println("Running...");
+		game.start();
 	}
 
 }
